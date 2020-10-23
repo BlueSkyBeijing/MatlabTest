420a03cd0f825d2dc588840e15aeff5c8cb39e11
chenjunlong@kingsoft.com <chenjunlong@kingsoft.com>
2020-10-23 09:18:50 +0800

    test plot property

--- Test.m
+++ Test.m
@@ -1,13 +1,22 @@
 clear all
+% plot
 y = [-10, 2, 3 , 4, 8, -10]
 plot(y)
 
+% plot2
 x1 = [-10, 2, 3 , 4, 8, -10]
 y1 = [10, 21, -3 , 14, 18, 10]
 plot(x1,y1)
 
+% plot3
 t=0:pi/360:2*pi;
 x=sin(t);
 y=cos(t);
 z=2*x.^2+y.^2;
 plot3(x,y,z)
+
+% plot property
+title("»˝Œ¨Õº–Œ")
+xlabel("x÷·")
+ylabel("y÷·")
+zlabel("z÷·")
